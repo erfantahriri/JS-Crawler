58ec9c6fba83fea44d5267ab5dfb902bfaa83d4a
Erfan Tahriri <erfan.tahriri@epidemicsound.com>
2023-02-12 18:42:45 +0100

    Add getURLsFromHTML and it's test

--- crawler.js
+++ crawler.js
@@ -1,8 +1,39 @@
+const { JSDOM } = require("jsdom");
+
 const linkNormalizer = (url) => {
   const urlObj = new URL(url);
-  return `${urlObj.hostname}${urlObj.pathname}`;
+  const hostPath = `${urlObj.hostname}${urlObj.pathname}`;
+  if (hostPath.length > 0 && hostPath.slice(-1) === "/") {
+    return hostPath.slice(0, -1);
+  }
+  return hostPath;
+};
+
+const getURLsFromHTML = (htmlBody, baseURL) => {
+  const dom = new JSDOM(htmlBody);
+  const links = dom.window.document.querySelectorAll("a");
+  const urls = [];
+  for (const link of links) {
+    if (link.href.slice(0, 1) === "/") {
+      try {
+        const urlObj = new URL(`${baseURL}${link.href}`);
+        urls.push(urlObj.href);
+      } catch (err) {
+        console.log(`error with relative url: ${err.message}`);
+      }
+    } else {
+      try {
+        const urlObj = new URL(link.href);
+        urls.push(urlObj.href);
+      } catch (err) {
+        console.log(`error with absolute url: ${err.message}`);
+      }
+    }
+  }
+  return urls;
 };
 
 module.exports = {
   linkNormalizer,
+  getURLsFromHTML,
 };
--- crawler.test.js
+++ crawler.test.js
@@ -1,8 +1,97 @@
-const { linkNormalizer } = require("./crawler");
+const { linkNormalizer, getURLsFromHTML } = require("./crawler");
 const { test, expect } = require("@jest/globals");
 
 test("linkNormalize", () => {
-  const actual = linkNormalizer("https://mehdy.me/en/");
-  const expected = "mehdy.me/en/";
+  const actual = linkNormalizer("https://mehdy.me");
+  const expected = "mehdy.me";
+  expect(actual).toEqual(expected);
+});
+
+test("linkNormalize path", () => {
+  const actual = linkNormalizer("https://mehdy.me/en");
+  const expected = "mehdy.me/en";
+  expect(actual).toEqual(expected);
+});
+
+test("linkNormalize slash", () => {
+  const actual = linkNormalizer("https://mehdy.me/");
+  const expected = "mehdy.me";
+  expect(actual).toEqual(expected);
+});
+
+test("linkNormalize uppercase", () => {
+  const actual = linkNormalizer("https://MEHDY.me");
+  const expected = "mehdy.me";
+  expect(actual).toEqual(expected);
+});
+
+test("getURLsFromHTML", () => {
+  const actual = getURLsFromHTML(`
+    <html>
+        <body>
+            <a href="https://mehdy.me/path1">
+                Path1
+            </a>
+        </body>
+    </html>
+    `);
+  const expected = ["https://mehdy.me/path1"];
+  expect(actual).toEqual(expected);
+});
+
+test("getURLsFromHTML relative", () => {
+  const baseURL = "https://mehdy.me";
+  const actual = getURLsFromHTML(
+    `
+      <html>
+          <body>
+              <a href="/path1">
+                  Path1
+              </a>
+          </body>
+      </html>
+      `,
+    baseURL
+  );
+  const expected = ["https://mehdy.me/path1"];
+  expect(actual).toEqual(expected);
+});
+
+test("getURLsFromHTML both", () => {
+  const baseURL = "https://mehdy.me";
+  const actual = getURLsFromHTML(
+    `
+        <html>
+            <body>
+                <a href="/path1/">
+                    Path1
+                </a>
+                <a href="https://mehdy.me/path2/">
+                    Path2
+                </a>
+            </body>
+        </html>
+        `,
+    baseURL
+  );
+  const expected = ["https://mehdy.me/path1/", "https://mehdy.me/path2/"];
+  expect(actual).toEqual(expected);
+});
+
+test("getURLsFromHTML both", () => {
+  const baseURL = "https://mehdy.me";
+  const actual = getURLsFromHTML(
+    `
+          <html>
+              <body>
+                  <a href="invalid">
+                      Path1
+                  </a>
+              </body>
+          </html>
+          `,
+    baseURL
+  );
+  const expected = [];
   expect(actual).toEqual(expected);
 });
