c07740128f466f249f6b46e2e079570a91e83995
Erfan Tahriri <erfan.tahriri@epidemicsound.com>
2023-02-12 19:56:09 +0100

    Implement the main function functionality

--- main.js
+++ main.js
@@ -1,3 +1,28 @@
-import { linkNormalizer } from "./crawler";
+const { crawlPage } = require("./crawler");
+const { printReport } = require("./report");
 
-linkNormalizer("testURL");
+const main = async () => {
+  if (process.argv.length < 3) {
+    console.log("no website provided");
+    process.exit(1);
+  }
+  if (process.argv.length > 3) {
+    console.log("too many command line args");
+    process.exit(1);
+  }
+
+  let baseURL = process.argv[2];
+  try {
+    new URL(baseURL);
+  } catch (err) {
+    console.log(`erro with provided website: ${err.message}`);
+    process.exit(1);
+  }
+
+  console.log("starting crawl");
+  const pages = await crawlPage(baseURL, baseURL, {});
+
+  printReport(pages);
+};
+
+main();
